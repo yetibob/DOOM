948d30b349a8130f5c7ff7b8f367fd0cd8e0d442
Wickie Lee <wickie.leejr@protonmail.com>
2023-04-20 15:17:49 -0400

    run clang-format

--- linuxdoom-1.10/wi_stuff.h
+++ linuxdoom-1.10/wi_stuff.h
@@ -1,4 +1,4 @@
-// Emacs style mode select   -*- C++ -*- 
+// Emacs style mode select   -*- C++ -*-
 //-----------------------------------------------------------------------------
 //
 // $Id:$
@@ -22,14 +22,13 @@
 #ifndef __WI_STUFF__
 #define __WI_STUFF__
 
-//#include "v_video.h"
+// #include "v_video.h"
 
 #include "doomdef.h"
 
 // States for the intermission
 
-typedef enum
-{
+typedef enum {
     NoState = -1,
     StatCount,
     ShowNextLoc
@@ -37,14 +36,14 @@ typedef enum
 } stateenum_t;
 
 // Called by main loop, animate the intermission.
-void WI_Ticker (void);
+void WI_Ticker(void);
 
 // Called by main loop,
 // draws the intermission directly into the screen buffer.
-void WI_Drawer (void);
+void WI_Drawer(void);
 
 // Setup for an intermission screen.
-void WI_Start(wbstartstruct_t*	 wbstartstruct);
+void WI_Start(wbstartstruct_t* wbstartstruct);
 
 #endif
 //-----------------------------------------------------------------------------
